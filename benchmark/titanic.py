822841461b8a8254e77a33d11dd7b1e633ec39ca
Haifeng Jin <jin@tamu.edu>
2020-11-03 22:45:20 +0000

    update

--- benchmark/titanic.py
+++ benchmark/titanic.py
@@ -14,10 +14,10 @@ TEST_DATA_URL = "https://storage.googleapis.com/tf-datasets/titanic/eval.csv"
 def main(output_file_path):
     train_file_path = tf.keras.utils.get_file("train.csv", TRAIN_DATA_URL)
     test_file_path = tf.keras.utils.get_file("eval.csv", TEST_DATA_URL)
-    clf = ak.StructuredDataClassifier(max_trials=1, directory='tmp_dir', overwrite=True)
+    clf = ak.StructuredDataClassifier(max_trials=10, directory='tmp_dir', overwrite=True)
 
     start_time = timeit.default_timer()
-    clf.fit(train_file_path, 'survived', epochs=1)
+    clf.fit(train_file_path, 'survived')
     stop_time = timeit.default_timer()
 
     accuracy = clf.evaluate(test_file_path, 'survived')[1]
